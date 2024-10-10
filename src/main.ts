6f2b6598ea73bdfb166dc9be95be22213ee81912
Trish Nguyen <tnguy510@ucsc.edu>
2024-10-09 22:59:00 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My Chrome game";
+const gameName = "My Web game";
 document.title = gameName;
 
+//Button Section
+const bigButton = document.createElement('bigButton');
+const bigButtonText = 'Button Check💪';
+bigButton.style.backgroundColor = 'grey';
+bigButton.style.color = 'black';
+//document.body.appendChild(bigButton);
+bigButton.addEventListener('click', () => {
+    console.log('button click');
+});
+
+//Header Section
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//const button = document.createElement("button1");
+bigButton.innerHTML = bigButtonText;
+app.append(bigButton);
