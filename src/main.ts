fd5bca2dc262be3f4e6b40548943a0b4d7e03a1a
Trish Nguyen <tnguy510@ucsc.edu>
2024-10-10 23:48:36 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ const gameName = "My Web game";
 document.title = gameName;
 
 let flexCounter: number = 0;
+//let currentTime: number = performance.timeOrigin + performance.now();
+let lastUpdatedTime: number;
 
 //Button Section
 const bigButton = document.createElement("button");
@@ -21,20 +23,35 @@ flexCounterDisplay.innerHTML = flexCounterText;
 app.append(flexCounterDisplay);
 
 function modifyCounterText() {
-    flexCounter++;
+  flexCounter++;
   flexCounterText = "Flex Counter: " + flexCounter;
 
   flexCounterDisplay.innerHTML = flexCounterText;
   app.append(flexCounterDisplay);
 }
 
-setInterval(modifyCounterText, 1000);
+//setInterval(modifyCounterText, 1000);
+
+function moveTime(timestamp: number){
+  if(lastUpdatedTime === undefined){
+    lastUpdatedTime = timestamp;
+  }
+  if(timestamp - lastUpdatedTime >= 1000){
+    lastUpdatedTime = timestamp;
+    console.log("I am inside the if statement");
+    modifyCounterText();
+  }
+  //currentTime = performance.timeOrigin + performance.now();
+  //console.log("Current difference is: " + (timestamp - lastUpdatedTime));
+  requestAnimationFrame(moveTime);
+}
 
 bigButton.addEventListener("click", () => {
   modifyCounterText();
   //console.log("button click " + flexCounterText);
 });
 
+requestAnimationFrame(moveTime);
 //Header Section
 const header = document.createElement("h1");
 header.innerHTML = gameName;
