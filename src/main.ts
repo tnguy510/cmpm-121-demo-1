552b8e8f78e49cb80f66df867ef73fcefe121b66
Trish Nguyen <tnguy510@ucsc.edu>
2024-10-13 00:30:41 -0700

    Step 8 complete

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My Web game";
+const gameName = "Arm Workout Simulator";
 document.title = gameName;
+
+//Header Section
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
 
 let flexCounter: number = 0.0;
 let flexRate: number = 0.0;
@@ -19,84 +24,96 @@ let upgrade1Price: number = 10;
 let upgrade2Price: number = 100;
 let upgrade3Price: number = 1000;
 
-//Test section
-//const bottomHalf = document.createElement('div');
-//bottomHalf.setAttribute("style", " max-width: 1280px; margin: 0 auto; text-align: left;");
+//Div section
+const bottomLeftHalf = document.createElement('div');
+bottomLeftHalf.style.setProperty('position', 'fixed')
+bottomLeftHalf.style.setProperty('bottom', '10px')
+bottomLeftHalf.style.setProperty('left', '10px')
+app.append(bottomLeftHalf);
+
+const bottomRightHalf = document.createElement('div');
+bottomRightHalf.style.setProperty('position', 'fixed')
+bottomRightHalf.style.setProperty('bottom', '10px')
+bottomRightHalf.style.setProperty('right', '10px')
+app.append(bottomRightHalf);
 
 //Button Section
 const bigButton = document.createElement("button");
-const bigButtonText = "Flex Check💪";
+const bigButtonText = "💪";
+bigButton.style.setProperty('font-size', '5em');
+bigButton.style.setProperty('position', 'fixed')
+bigButton.style.setProperty('top', '0')
 
 bigButton.innerHTML = bigButtonText;
 app.append(bigButton);
 
 //Upgrade Button 1
 const upgradeButtonTier1 = document.createElement("button");
-const upgradeButtonTier1Text = "Automatic Reps 1";
+const upgradeButtonTier1Text = "Fish Jerky";
 
 upgradeButtonTier1.innerHTML = upgradeButtonTier1Text;
-app.append(upgradeButtonTier1);
+bottomLeftHalf.append(upgradeButtonTier1);
 upgradeButtonTier1.disabled = true;
 
 //1st Button Counter
 const upgradeButtonTier1Amount = document.createElement("flexes");
-let upgradeButtonTier1TextAmount = "Automatic Reps 1 Amount: " + upgrade1Amount;
+let upgradeButtonTier1TextAmount = "Fish Jerky Bought: " + upgrade1Amount;
 
 upgradeButtonTier1Amount.innerHTML = upgradeButtonTier1TextAmount;
-app.append(upgradeButtonTier1Amount);
+bottomRightHalf.append(upgradeButtonTier1Amount);
 
 //The Second Button
 const upgradeButtonTier2 = document.createElement("button");
-const upgradeButtonTier2Text = "Automatic Reps 2";
+const upgradeButtonTier2Text = "Protein Shakes";
 
 upgradeButtonTier2.innerHTML = upgradeButtonTier2Text;
-app.append(upgradeButtonTier2);
+bottomLeftHalf.append(upgradeButtonTier2);
 upgradeButtonTier2.disabled = true;
 
 //2nd Button Counter
 const upgradeButtonTier2Amount = document.createElement("flexes");
-let upgradeButtonTier2TextAmount = "Automatic Reps 2 Amount: " + upgrade2Amount;
+let upgradeButtonTier2TextAmount = "Protein Shakes Bought: " + upgrade2Amount;
 
 upgradeButtonTier2Amount.innerHTML = upgradeButtonTier2TextAmount;
-app.append(upgradeButtonTier2Amount);
+bottomRightHalf.append(upgradeButtonTier2Amount);
 
 //The Third Button
 const upgradeButtonTier3 = document.createElement("button");
-const upgradeButtonTier3Text = "Automatic Reps 3";
+const upgradeButtonTier3Text = "Steroids";
 
 upgradeButtonTier3.innerHTML = upgradeButtonTier3Text;
-app.append(upgradeButtonTier3);
+bottomLeftHalf.append(upgradeButtonTier3);
 upgradeButtonTier3.disabled = true;
 
 //3rd Button Counter
 const upgradeButtonTier3Amount = document.createElement("flexes");
-let upgradeButtonTier3TextAmount = "Automatic Reps 3 Amount: " + upgrade3Amount;
+let upgradeButtonTier3TextAmount = "Steroids Bought: " + upgrade3Amount;
 
 upgradeButtonTier3Amount.innerHTML = upgradeButtonTier3TextAmount;
-app.append(upgradeButtonTier3Amount);
+bottomRightHalf.append(upgradeButtonTier3Amount);
 
 //Flex Counter Display
 const flexCounterDisplay = document.createElement("flexes");
-let flexCounterText = "Flex Counter: " + flexCounter;
+let flexCounterText = "Flexes Completed: " + flexCounter;
 
 flexCounterDisplay.innerHTML = flexCounterText;
 app.append(flexCounterDisplay);
 
 //Upgrade Modify Text
 function modifyUpgradeText() {
-  upgradeButtonTier1TextAmount = "Automatic Reps 1 Amount: " + upgrade1Amount;
+  upgradeButtonTier1TextAmount = "Fish Jerky Bought: " + upgrade1Amount;
   upgradeButtonTier1Amount.innerHTML = upgradeButtonTier1TextAmount;
 
-  upgradeButtonTier2TextAmount = "Automatic Reps 2 Amount: " + upgrade2Amount;
+  upgradeButtonTier2TextAmount = "Protein Shakes Bought: " + upgrade2Amount;
   upgradeButtonTier2Amount.innerHTML = upgradeButtonTier2TextAmount;
 
-  upgradeButtonTier3TextAmount = "Automatic Reps 3 Amount: " + upgrade3Amount;
+  upgradeButtonTier3TextAmount = "Steroids Bought: " + upgrade3Amount;
   upgradeButtonTier3Amount.innerHTML = upgradeButtonTier3TextAmount;
 }
 
 //Counter Text Functions
 function modifyCounterText() {
-  flexCounterText = "Flex Counter: " + flexCounter;
+  flexCounterText = "Flexes Completed: " + flexCounter;
   flexCounterDisplay.innerHTML = flexCounterText;
   //app.append(flexCounterDisplay);
 }
@@ -173,7 +190,7 @@ upgradeButtonTier1.addEventListener("click", () => {
   upgrade1Amount++;
   upgrade1Price *= 1.15;
   upgrade1Price = Math.round(upgrade1Price * 100) / 100;
-  console.log(upgrade1Price);
+  //console.log(upgrade1Price);
   modifyUpgradeText();
   statusTextDisplay();
   modifyCounterText();
@@ -184,7 +201,8 @@ upgradeButtonTier2.addEventListener("click", () => {
   flexRate += 2;
   upgrade2Amount++;
   upgrade2Price *= 1.15;
-  upgrade2Price = Math.round(upgrade1Price * 100) / 100;
+  upgrade2Price = Math.round(upgrade2Price * 100) / 100;
+  console.log(upgrade2Price)
   modifyUpgradeText();
   statusTextDisplay();
   modifyCounterText();
@@ -195,14 +213,10 @@ upgradeButtonTier3.addEventListener("click", () => {
   flexRate += 50;
   upgrade3Amount++;
   upgrade3Price *= 1.15;
-  upgrade3Price = Math.round(upgrade1Price * 100) / 100;
+  upgrade3Price = Math.round(upgrade3Price * 100) / 100;
   modifyUpgradeText();
   statusTextDisplay();
   modifyCounterText();
 });
 
 requestAnimationFrame(moveTime);
-//Header Section
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
